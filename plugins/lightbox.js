49b0fcb5a1ec39d2c7094841b06cdffc59e89005
Ariel Balter <ariel@arielbalter.com>
2024-09-17 12:22:05 -0700

    implemented info pages. needs styling

--- plugins/lightbox.js
+++ plugins/lightbox.js
@@ -1,6 +1,6 @@
 export default function attachLightboxListeners(event, targetElement) {
-    console.log({targetElement});
-    
+    console.log({ targetElement });
+
     console.log("Lightbox trigger clicked");
 
     const lightbox = document.querySelector('.lightbox');
@@ -15,58 +15,75 @@ export default function attachLightboxListeners(event, targetElement) {
     lightbox_caption.innerHTML = caption_text; // Set the lightbox caption
     lightbox.classList.add('open'); // Show the lightbox
 
+    // Attempt to enter fullscreen mode
+    if (lightbox.requestFullscreen) {
+        lightbox.requestFullscreen();
+    } else if (lightbox.webkitRequestFullscreen) { /* Safari */
+        lightbox.webkitRequestFullscreen();
+    } else if (lightbox.msRequestFullscreen) { /* IE11 */
+        lightbox.msRequestFullscreen();
+    }
+
     // Close the lightbox when clicking on the lightbox itself (outside the image)
     lightbox.addEventListener(
         'click',
         function () {
             // console.log("Lightbox clicked, closing...");
             lightbox.classList.remove('open'); // Close the lightbox
+
+            if (document.fullscreenElement) {
+                document.exitFullscreen();
+            } else if (document.webkitFullscreenElement) { /* Safari */
+                document.webkitExitFullscreen();
+            } else if (document.msFullscreenElement) { /* IE11 */
+                document.msExitFullscreen();
+            }
         },
         { once: true }
     ); // Use { once: true } to ensure the listener is removed after one click
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    const lightbox = document.querySelector('.lightbox');
-    const lightbox_img = document.querySelector('#lightbox-img');
-    const lightbox_caption = document.querySelector('#lightbox-caption');
+// document.addEventListener("DOMContentLoaded", () => {
+//     const lightbox = document.querySelector('.lightbox');
+//     const lightbox_img = document.querySelector('#lightbox-img');
+//     const lightbox_caption = document.querySelector('#lightbox-caption');
 
-    const figures = document.querySelectorAll('.details-figure');
+//     const figures = document.querySelectorAll('.details-figure');
 
-    figures.forEach(figure => {
-        figure.addEventListener('click', (e) => {
-            const img = figure.querySelector('img');
-            const img_src = img ? img.src : '';
-            const caption = figure.querySelector('figcaption');
-            const caption_text = caption ? caption.innerHTML : '';
+//     figures.forEach(figure => {
+//         figure.addEventListener('click', (e) => {
+//             const img = figure.querySelector('img');
+//             const img_src = img ? img.src : '';
+//             const caption = figure.querySelector('figcaption');
+//             const caption_text = caption ? caption.innerHTML : '';
 
-            lightbox_img.src = img_src;
-            lightbox_caption.innerHTML = caption_text;
+//             lightbox_img.src = img_src;
+//             lightbox_caption.innerHTML = caption_text;
 
-            // Show the lightbox
-            lightbox.classList.add('open');
+//             // Show the lightbox
+//             lightbox.classList.add('open');
 
-            // Attempt to enter fullscreen mode
-            if (lightbox.requestFullscreen) {
-                lightbox.requestFullscreen();
-            } else if (lightbox.webkitRequestFullscreen) { /* Safari */
-                lightbox.webkitRequestFullscreen();
-            } else if (lightbox.msRequestFullscreen) { /* IE11 */
-                lightbox.msRequestFullscreen();
-            }
-        });
-    });
+//             // Attempt to enter fullscreen mode
+//             if (lightbox.requestFullscreen) {
+//                 lightbox.requestFullscreen();
+//             } else if (lightbox.webkitRequestFullscreen) { /* Safari */
+//                 lightbox.webkitRequestFullscreen();
+//             } else if (lightbox.msRequestFullscreen) { /* IE11 */
+//                 lightbox.msRequestFullscreen();
+//             }
+//         });
+//     });
 
-    // Close the lightbox and exit fullscreen mode
-    lightbox.addEventListener('click', function () {
-        lightbox.classList.remove('open');
+//     // Close the lightbox and exit fullscreen mode
+//     lightbox.addEventListener('click', function () {
+//         lightbox.classList.remove('open');
 
-        if (document.fullscreenElement) {
-            document.exitFullscreen();
-        } else if (document.webkitFullscreenElement) { /* Safari */
-            document.webkitExitFullscreen();
-        } else if (document.msFullscreenElement) { /* IE11 */
-            document.msExitFullscreen();
-        }
-    });
-});
+//         if (document.fullscreenElement) {
+//             document.exitFullscreen();
+//         } else if (document.webkitFullscreenElement) { /* Safari */
+//             document.webkitExitFullscreen();
+//         } else if (document.msFullscreenElement) { /* IE11 */
+//             document.msExitFullscreen();
+//         }
+//     });
+// });
